55e8036e6f40f27d556db266fc6d350d9bc93c43
tsGlow <haruki84@msn.com>
2026-07-21 10:15:42 +0900

    basic design for menubars done

--- scrapper/static/scrapper/index.js
+++ scrapper/static/scrapper/index.js
@@ -1,6 +1,6 @@
 
-// vars
-const scrBtn = document.getElementById('scrapBtn');
+// vars from doms
+const scrapBtn = document.getElementById('scrapBtn');
 const loadDiv = document.getElementById('loader');
 const contentDiv = document.getElementById('content');
 const menuTabs = document.getElementById('menuTabs');
@@ -9,9 +9,19 @@ const searchNews = document.getElementById('searchNews');
 const searchBar = document.getElementById('searchBar');
 
 
+// template for html rendering
+const IndividualNewsTemplate = '<h3>${dict.cat} | <a href="${dict.link}" target="_blank">${dict.title}</a> - ${dict.media}</h3><h4>${dict.pubDate} | <a href="${dict.link}" target="_blank">${dict.link}</a></h4><h4><i>기사 요약</i> <br>${dict.description}</h4><h4><i>기사 전문</i> <br>${dict.text}</h4><br/><br/>';
+
+
 // functions
+function templateToHtml(dict,template){
+    return template.replace(/\$\{dict\.([^}]+)\}/g, function(match, key) {
+        return dict[key] !== undefined ? dict[key] : '';});
+}
+
+
 async function getJson(url, contextName){
-    const res = await fetch(url)
+    const res = await fetch(url);
     const data = await res.json();        
     if (res.ok){        
         return data[contextName];
@@ -20,19 +30,15 @@ async function getJson(url, contextName){
     };                                
 }
 
+
 function renderNews(array,parentElement){
     array.forEach(item => {
         const articleDiv = document.createElement('div');
-        articleDiv.innerHTML = `
-            <h3>${item.cat} | <a href="${item.link}" target="_blank">${item.title}</a> - ${item.media}</h3>
-            <h4>${item.pubDate} | <a href="${item.link}" target="_blank">${item.link}</a></h4>      
-            <h4><i>기사 요약</i> <br>${item.description}</h4>
-            <h4><i>기사 전문</i> <br>${item.text}</h4>
-            <br/><br/>
-            `;
-        parentElement.appendChild(articleDiv)
+        articleDiv.innerHTML = templateToHtml(item,IndividualNewsTemplate);
+        parentElement.appendChild(articleDiv);
     });
 }
+
 
 function invertDisplay(element){
     if (element.style.display === 'none') {
@@ -44,23 +50,54 @@ function invertDisplay(element){
 }
 
 
+
+function swapActivity(el1, el2, content1, content2){    
+    if (el1.className === "deactive"){
+        el1.className = 'active';
+        el2.className = 'deactive';
+        content1.style.display = 'block';
+        content2.style.display = 'none';
+
+    };
+    /*
+    const el1Style = window.getComputedStyle(el1);
+    const el2Style = window.getComputedStyle(el2);
+
+    const tempBg =  el1Style.getPropertyValue("background-color");
+    const tempColor =  el1Style.getPropertyValue("color");
+
+    el1.style.backgroundColor = el2Style.getPropertyValue("background-color");
+    el1.style.color = el2Style.getPropertyValue("color");
+
+    el2.style.backgroundColor = tempBg;
+    el2.style.color = tempColor;
+
+    el2.removeEventListener("click", () => swapStyles(el1,el2));
+    el1.addEventListener("click", () => swapStyles(el2, el1));
+    */
+}
+
+
+
+
+
 // listeners
-scrBtn.addEventListener("click", async function(){
+scrapBtn.addEventListener("click", async function(){
     
-    invertDisplay(loadDiv);
-    console.log(menuTabs.style.display);
-    invertDisplay(menuTabs);
-    console.log(menuTabs.style.display);
+    invertDisplay(loadDiv);    
+    invertDisplay(menuTabs);    
     invertDisplay(contentDiv);
 
     contentDiv.innerHTML = '';
     const newsJson = await getJson('scrap-news/','article');
     await renderNews(newsJson,contentDiv);
-    todayNews.innerText = `News Today (총 ${newsJson.length}개의 기사)`
+    todayNews.innerText = `News Today (총 ${newsJson.length}개의 기사)`;
 
     invertDisplay(loadDiv);
     invertDisplay(menuTabs);
     invertDisplay(contentDiv);
-
 })
 
+
+searchNews.addEventListener("click", () => swapActivity(searchNews,todayNews,searchBar,contentDiv));
+todayNews.addEventListener("click", () => swapActivity(todayNews,searchNews,contentDiv,searchBar));
